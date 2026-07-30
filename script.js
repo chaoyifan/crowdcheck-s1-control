5f33779acc258650f9feaea294672b8956cc17c8
chaoyifan <52701868+chaoyifan@users.noreply.github.com>
2026-07-30 21:13:47 +0800

    Load post-publication editing flow

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 (() => {
-  const script = document.createElement("script");
-  script.src = "https://chaoyifan.github.io/crowdcheck-s1-control/study1-app.js?v=d459af4b0b99700115e9a084bfc3c2434f090d5d";
-  script.async = false;
-  document.head.appendChild(script);
+  const app = document.createElement("script");
+  app.src = "https://chaoyifan.github.io/crowdcheck-s1-control/study1-app.js?v=d459af4b0b99700115e9a084bfc3c2434f090d5d";
+  app.async = false;
+  app.onload = () => {
+    const editing = document.createElement("script");
+    editing.src = "https://chaoyifan.github.io/crowdcheck-s1-control/study1-post-edit.js?v=20260730a";
+    editing.async = false;
+    document.head.appendChild(editing);
+  };
+  document.head.appendChild(app);
 })();
